6a625eb158c42509df04a8200b628e3b8aa1eb68
giorgi <jalala18@mail.ru>
2024-12-29 19:15:26 +0400

    update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const jsonServer = require('json-server')
 const path = require('path')
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, '../src/db.json'))
+const router = jsonServer.router('../src/db.json')
+// const router = jsonServer.router(path.join(__dirname, '../src/db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
